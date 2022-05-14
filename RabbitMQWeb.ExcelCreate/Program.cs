dad8ad2be2c47e72e76a0763f12cbb3737e407de
dogustuluk <dogus.tuluk@gmail.com>
2022-05-14 22:23:06 +0300

    seed datas

--- RabbitMQWeb.ExcelCreate/Program.cs
+++ RabbitMQWeb.ExcelCreate/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQWeb.ExcelCreate.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +17,26 @@ namespace RabbitMQWeb.ExcelCreate
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                //kullanýcý kaydetmek için UserManager almamýz lazým
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                appDbContext.Database.Migrate(); //database-update yazmayý unutursak uygulama ayaða kalkarken ilgili komutlarý veritabanýna iþlemesini saðlayan kod.
+
+                if (!appDbContext.Users.Any())
+                {
+                    userManager.CreateAsync(new IdentityUser { UserName = "dogus", Email = "dogus@gmail.com" }, "Password12*").Wait();
+                    userManager.CreateAsync(new IdentityUser { UserName = "Dogus2", Email = "Dogus2Tuluk@gmail.com" }, "Password12*").Wait();
+                }
+            }
+            
+                
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
