a09c175d547700dab2a0f585752b4bfe659044df
dogustuluk <dogus.tuluk@gmail.com>
2022-05-14 22:38:04 +0300

    accountController and login page design

---
+++ RabbitMQWeb.ExcelCreate/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQWeb.ExcelCreate.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Email, string Password)
+        {
+            return View();
+        }
+    }
+}
