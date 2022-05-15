969a74012d64cd40178e919f13774367909423a0
dogustuluk <dogus.tuluk@gmail.com>
2022-05-15 15:51:08 +0300

    Shared > CreateExcelMessage class created

---
+++ Shared/CreateExcelMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class CreateExcelMessage
+    {
+        public string UserId { get; set; }
+        public int FileId { get; set; }
+
+        //public List<Product> Products { get; set; } //çok fazla data olduğu için data'ları mesajın içerisine gömmemeliyiz. Dataları worker service'in veritabanı ile iletişim kurmasıyla dosyanın oluşturulması lazım
+    }
+}
